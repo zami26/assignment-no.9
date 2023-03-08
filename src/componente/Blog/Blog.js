eeeb1daa9f5c1c5a4886050b92d56ff24c94e5f8
zami26 <arafatulislamzami@gmail.com>
2023-03-08 12:07:58 +0600

    8th commit

--- src/componente/Blog/Blog.js
+++ src/componente/Blog/Blog.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h3>This is blog page.</h3>
+        <div className='container'>
+            <h3 className='my-5'>Welcome to blog page.</h3>
+            <div className='shadow p-3 my-5 bg-body-tertiary rounded  text-start'>
+                <p className='fw-semibold'>1. What is the purpose of react router? </p>
+                <p className='ms-3'> <span className='fw-bold'>Ans. </span>
+                    React Router is a JavaScript framework that lets us handle client and server-side routing in React applications. It enables the creation of single-page web or mobile apps that allow navigating without refreshing the page. It also allows us to use browser history features while preserving the right application view.
+                </p>
+
+            </div>
+            <div className='shadow p-3 my-5 bg-body-tertiary rounded  text-start'>
+                <p className='fw-semibold'>
+                    2. How does context API works?
+                </p>
+                <p className='ms-3'> <span className='fw-bold'>Ans. </span>
+                    The Context API helps share data between components which you can't easily share with props, for example, complex data objects. React Context API provides a way to send data like userId, auth, and theme data through the component tree without sending any props manually at every level.</p>
+            </div>
+            <div className='shadow p-3 my-5 bg-body-tertiary rounded  text-start'>
+                <p>3. What is "useRef" hook in react js</p>
+                <p className='ms-3'> <span className='fw-bold'>Ans. </span>The useRef is a hook that allows to directly create a reference to the DOM element in the functional component.The useRef returns a mutable ref object. This object has a property called .current. The value is persisted in the refContainer.current property. These values are accessed from the current property of the returned object. The .current property could be initialised to the passed argument initialValue e.g. useRef(initialValue). The object can persist a value for a full lifetime of the component.
+
+                </p>
+            </div>
         </div>
     );
 };
--- src/componente/Question/Question.js
+++ src/componente/Question/Question.js
@@ -3,9 +3,17 @@ import './Question.css'
 import { EyeIcon } from '@heroicons/react/24/solid'
 
 const Question = ({ allQuestion }) => {
-    console.log(allQuestion);
+    // console.log(allQuestion);
     const { question, options, correctAnswer } = allQuestion;
-    const [answer, setAnswer] = useState([]);
+    const [answer, setAnswer] = useState();
+    const getSelectedAnswer = (event) => {
+        setAnswer(event.target.value);
+        // console.log(answer);
+    }
+    const showCorrectAnswer = () => {
+        const answer = { correctAnswer };
+        alert(answer.correctAnswer)
+    };
 
 
     return (
@@ -13,35 +21,30 @@ const Question = ({ allQuestion }) => {
             <div className='question shadow p-3 mb-5 bg-body-tertiary rounded'>
                 <div className='d-flex justify-content-between'>
                     <p>quiz: {question}</p>
-                    <EyeIcon className="icon text-blue-500" />
+                    <EyeIcon onClick={showCorrectAnswer} className="icon text-blue-500" />
+
+
+
                 </div>
                 <div>
                     <div className='row'>
                         <div className="form-check option col me-3">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
-                            <label className="form-check-label" >
-                                {options[0]}
-                            </label>
+                            <input className="form-check-input" type="radio" name="flexRadioDefault" value={answer} id="flexRadioDefault1" onChange={getSelectedAnswer} />
+                            {options[0]}
                         </div>
                         <div className="form-check option col">
                             <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                            <label className="form-check-label" >
-                                {options[1]}
-                            </label>
+                            {options[1]}
                         </div>
                     </div>
                     <div className="row">
                         <div className="form-check option col me-3 mt-2">
                             <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                            <label className="form-check-label" >
-                                {options[2]}
-                            </label>
+                            {options[2]}
                         </div>
                         <div className="form-check option col mt-2">
                             <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                            <label className="form-check-label" >
-                                {options[3]}
-                            </label>
+                            {options[3]}
                         </div>
                     </div>
                 </div>
